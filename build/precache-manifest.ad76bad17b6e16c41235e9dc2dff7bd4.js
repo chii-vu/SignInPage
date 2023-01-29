cc3e177a75db0085b0821996c82be2cd33e4b440
Chi <115325256+chii-vu@users.noreply.github.com>
2023-01-29 03:15:09 +0000

    Initial Commit

---
+++ build/precache-manifest.ad76bad17b6e16c41235e9dc2dff7bd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9c88453c4f4373f4002905de43f1fa",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f71e69c1288b4a4eda23",
+    "url": "/myapp/static/css/main.5a926c7a.chunk.css"
+  },
+  {
+    "revision": "b530a833a2a1987bb1f2",
+    "url": "/myapp/static/js/2.49ce2399.chunk.js"
+  },
+  {
+    "revision": "f71e69c1288b4a4eda23",
+    "url": "/myapp/static/js/main.adf9035f.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
